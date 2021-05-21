73b1910dba87a473e7947cdf2ca113c4ca0f6aec
Fendansas <fendansas@gmail.com>
2021-05-21 09:21:56 +0300

    Fix connect to db 2

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
     define('ROOT', dirname(__FILE__));
     require_once (ROOT.'/components/Router.php');
 
+    require_once (ROOT.'/components/Db.php');
+
 
     $router = new Router();
     $router->run();
--- models/News.php
+++ models/News.php
@@ -1,5 +1,4 @@
 <?php
-require 'components/Db.php';
 
 
     class News{
